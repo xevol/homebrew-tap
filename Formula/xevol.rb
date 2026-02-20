7294d82deecb39e5a777d9c2bf5abdb985472f1c
bigman <k@kuanysh.com>
2026-02-20 03:22:31 +0100

    Update xevol to 0.11.20

--- Formula/xevol.rb
+++ Formula/xevol.rb
@@ -1,23 +1,23 @@
 class Xevol < Formula
   desc "Command-line tool to consume, remix, make, publish, and offer systems, products, and workflows"
   homepage "https://xevol.com"
-  version "0.11.17"
+  version "0.11.20"
   license "UNLICENSED"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/xevol/xevol-cli/releases/download/v0.11.17/xevol-darwin-arm64.tar.gz"
-      sha256 "58373ca078a8337cf5bfbe40795ac3e9bd9fc1c8be1829c796e0b12ecde648b1"
+      url "https://github.com/xevol/xevol-cli/releases/download/v0.11.20/xevol-darwin-arm64.tar.gz"
+      sha256 "07b8cf26d557f14f3631be8b1b497404f9124ffc191e23a4ebd81d85af88ffd4"
     else
-      url "https://github.com/xevol/xevol-cli/releases/download/v0.11.17/xevol-darwin-x64.tar.gz"
-      sha256 "993b46701a185a382b15c0275e1791b6abb3e1a6d7ef6d0cd4bb8f275cf2fbef"
+      url "https://github.com/xevol/xevol-cli/releases/download/v0.11.20/xevol-darwin-x64.tar.gz"
+      sha256 "c7c9fdcdd1ad81c95f929f71b2390e6f3289c10e8f9679313232d73a55801e14"
     end
   end
 
   on_linux do
     depends_on arch: :x86_64
-    url "https://github.com/xevol/xevol-cli/releases/download/v0.11.17/xevol-linux-x64.tar.gz"
-    sha256 "f34685bc7918ae8109f84eab8729769953e710c69266e9867167368862bcf535"
+    url "https://github.com/xevol/xevol-cli/releases/download/v0.11.20/xevol-linux-x64.tar.gz"
+    sha256 "9aa64e9a9d9ad2015afdc3ce70b41b50a7eaf5daecd7f4619e6d898c73778dfe"
   end
 
   def install
@@ -35,7 +35,7 @@ class Xevol < Formula
   end
 
   test do
-    assert_match "0.11.17", shell_output("#{bin}/xevol --version")
+    assert_match "0.11.20", shell_output("#{bin}/xevol --version")
     assert_match "Xevol is a tool", shell_output("#{bin}/xevol --help")
   end
 end
